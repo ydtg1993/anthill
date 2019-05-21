5a301f2bb7789794681d5cb8a37c1203acf9fac7
ydtg199303 <946818508@qq.com>
2019-05-21 19:08:44 +0800

    *

--- main.go
+++ main.go
@@ -21,13 +21,13 @@ func main() {
 func getConf() (conf *src.ServerConfig, err error) {
 	content, err := ioutil.ReadFile("ini.json")
 	if err != nil {
-		return _, errors.New("can't open the config file")
+		return nil, errors.New("can't open the config file")
 	}
 
 	var config = new(src.ServerConfig)
 	json.Unmarshal(content, &config)
 	if err != nil {
-		return _, errors.New("explain json error")
+		return nil, errors.New("explain json error")
 	}
 
 	return config, nil
--- src/dispatcher.go
+++ src/dispatcher.go
@@ -3,16 +3,21 @@ package src
 import (
 	"golang.org/x/net/websocket"
 	"net"
+	"sync"
 )
 
 var TPool *TcpPool
 var WPool *WebsocketPool
+var WG sync.WaitGroup
 
 func Dispatch(config *ServerConfig) {
 	TPool = new(TcpPool)
 	TPool.Workers = make(map[string]net.Conn)
 	WPool = new(WebsocketPool)
 	WPool.Workers = make(map[string]*websocket.Conn)
+	WG.Add(2)
 	go TcpWorker(config)
 	go WebWorker(config)
+
+	WG.Wait()
 }
